797b77a900d91d70e1c8383e27684c3b50c76819
SKer001 <ppappapp8077616@gmail.com>
2022-03-24 00:27:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,12 @@ bot = commands.Bot(command_prefix='$')
 async def on_ready():
     print(">> BOT is online <<")
 #RUNbot
-bot.run("OTU2MjEwOTE5NDc2Njk1MDYw.Yjs60A.pFWDyR_aj6QoOzATS5hNUvBYyTs")
+bot.run("OTU2MjEwOTE5NDc2Njk1MDYw.Yjs60A.rC0ioNJICJ0AyATWBgXW1GO288U")
+
+@bot.event
+async def on_member_join(member):
+    print(f"[member] join!")
+
+@bot.event
+async def on_member_remove(member):
+    print(f"[member] leave!")
