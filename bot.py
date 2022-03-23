0914c6cf0f531b07f761a3c133c2fcda9e8edfd3
SKer001 <ppappapp8077616@gmail.com>
2022-03-24 00:09:59 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-print("Hello")
+import discord
+from discord.ext import commands
+#BOT指令為$
+bot = commands.Bot(command_prefix='$')
+#上線後回傳到黑窗
+@bot.event
+async def on_ready():
+    print(">> BOT is online <<")
+#RUNbot
+bot.run("OTU2MjEwOTE5NDc2Njk1MDYw.Yjs60A.pFWDyR_aj6QoOzATS5hNUvBYyTs")
