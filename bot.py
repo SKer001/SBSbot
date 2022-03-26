cef0d6b92d635aedd91935e80e9c67568bbf0794
SKer001 <ppappapp8077616@gmail.com>
2022-03-26 13:16:29 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,35 @@
-from sys import intern
+from http.client import ImproperConnectionState
 import discord
 from discord.ext import commands
+import json
+
+with open("setting.json",mode="r",encoding="utf8") as jfile:
+    jdata = json.load(jfile)
+
 #intens為特殊權限管理 member要額外開
-intens = discord.Intents.default()
-intens.members = True
+intents = discord.Intents.default()
+intents.members = True
 
 
 #BOT指令為$
-bot = commands.Bot(command_prefix='$',intens = intens)
+bot = commands.Bot(command_prefix='$',intents = intents)
 #上線後回傳到黑窗
 @bot.event
 async def on_ready():
     print(">> BOT is online <<")
-
-#diediediediediediediediediediediediediediediediediedie
+#BOT事件 加入與離開   
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(921239567611351093)
-    await channel.send(f"{member} join!")
-    print(f"{member} join!")                             #joun and leave 壞死
+    channel = bot.get_channel(int(jdata["Welcome_Channel"]))
+    await channel.send(f"@everyone {member} join!")
+    print(f"{member} join!")
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(921239567611351093)
-    await channel.send(f"{member} join!")
+    channel = bot.get_channel(int(jdata["Leave_Channel"]))
+    await channel.send(f"@everyone {member} leave!")
     print(f"{member} leave!")
-#diediediediediediediediediediediediediediediediediedie
-
+#  BOT$指令
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"{round(bot.latency*1000)} (ms)")
@@ -40,4 +43,4 @@ async def 早安(ctx):
     await ctx.send(f"早安")
 
 #RUNbot
-bot.run("OTU2MjEwOTE5NDc2Njk1MDYw.Yjs60A.nLYRWIUG3_INe-do5QweWZLAYvI")
+bot.run(jdata["TOKEN"])
