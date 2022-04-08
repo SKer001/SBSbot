c703eaf6e0fba80f6967b2d05ef8715ba38f7b87
SKer001 <ppappapp8077616@gmail.com>
2022-04-08 18:29:21 +0800

    Create react.py

---
+++ cmds/react.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+import random
+import json
+with open("setting.json",mode="r",encoding="utf8") as jfile:
+    jdata = json.load(jfile)
+
+class react(Cog_Extension):
+
+    @commands.command()
+    async def number5(self,ctx):
+        random_N5 = random.choice(jdata["N5"])
+        N5 = str(random_N5)
+        await ctx.send(N5)
+
+    @commands.command()
+    async def 早安(self,ctx):
+        await ctx.send(f"早安")
+
+    @commands.command()
+    async def 圖片(self,ctx):
+        random_pic = random.choice(jdata["pic"])
+        pic = discord.File(random_pic)
+        await ctx.send(file= pic)
+
+def setup(bot):
+    bot.add_cog(react(bot))
