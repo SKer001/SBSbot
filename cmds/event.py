d34740194fbf751d08cbbc05f67d5e6458337f03
SKer001 <ppappapp8077616@gmail.com>
2022-04-09 21:00:52 +0800

    Update event.py

--- cmds/event.py
+++ cmds/event.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from core.classes import Cog_Extension
 import random
 import json
-with open("setting.json",mode="r",encoding="utf8") as jfile:
+with open("../jfile/setting.json",mode="r",encoding="utf8") as jfile:
     jdata = json.load(jfile)
     
 class event(Cog_Extension):
@@ -21,13 +21,11 @@ class event(Cog_Extension):
 
     @commands.Cog.listener()
     async def on_message(self,msg):
-        if msg.content.endswith("崧瀚"):
-            await msg.channel.send(f"教官來了")
-
-    @commands.Cog.listener()
-    async def on_message(self,msg):
+        random_num5 = random.choice(jdata["number5"])
         if msg.content.endswith("早安"):
             await msg.channel.send(f"早ㄤ阿")
+        if msg.content.endswith("number5"):
+            await msg.channel.send(str(random_num5))
 
 def setup(bot):
     bot.add_cog(event(bot))
